e898b428a26221f9caef770e453615502a292f37
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2024-08-13 21:45:08 +0530

    vite shadcn ts setup

---
+++ client/src/pages/auth/index.tsx
@@ -0,0 +1,22 @@
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
+
+const Auth = () => {
+  return (
+    <div className="flex ">
+      <Card>
+        <CardHeader>
+          <CardTitle>Sign in</CardTitle>
+        </CardHeader>
+        <CardContent>
+       
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Auth;
